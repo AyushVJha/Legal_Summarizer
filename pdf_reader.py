38c935cbf6223bdc22f83932ae9935bcd538cd4d
Ayush Vardhan Jha <ayushvjha@gmail.com>
2025-05-01 09:34:43 +0530

    Update pdf_reader.py

--- pdf_reader.py
+++ pdf_reader.py
@@ -1,9 +1,5 @@
 # pdf_reader.py
-import fitz  # PyMuPDF
+from pdfminer.high_level import extract_text
 
 def pdf_to_text(file):
-    doc = fitz.open(stream=file.read(), filetype="pdf")
-    text = ""
-    for page in doc:
-        text += page.get_text() + "\n"
-    return text
+    return extract_text(file)
